4d30e2dcd9273e6df09b5e3a03f7a35929ea48f9
Nahidur Rahaman <nahidurrahaman4152gmail.com>
2025-12-29 13:10:36 +0600

    requirments added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="RAG_Medical_Chatbot",
+    version="0.1.0",
+    author="Nahidur Rahaman",
+    author_email="nahidurrahaman415@gmil.com",
+    packages=find_packages(),
+    install_requires=[]
+)
